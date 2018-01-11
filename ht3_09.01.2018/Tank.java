54b1be4aad63af7331b36c6518948b34ba68e1db
taniadruzh <tania.druzhchenko@gmail.com>
2018-01-11 21:40:36 +0200

    ht3

---
+++ ht3_09.01.2018/Tank.java
@@ -0,0 +1,29 @@
+public class Tank {
+    private int yearOfIssue;
+    private String model;
+    private float weight;
+
+    public int getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public void setYearOfIssue(int yearOfIssue) {
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
